af3874410d3e0098dcbb006df01bb5754f36fb25
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-13 21:02:43 +0300

    Format code

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,29 +1,27 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store/users/usersSlice";
 
 export const Users = () => {
-    const {users, isLoading, error } = useSelector(state => state.users);
-    const dispatch = useDispatch();
+  const { users, isLoading, error } = useSelector((state) => state.users);
+  const dispatch = useDispatch();
 
-    useEffect(_ => {
-        dispatch(fetchUsers())
-    },[])
+  useEffect((_) => {
+    dispatch(fetchUsers());
+  }, []);
   return (
     <div>
-        <p>{isLoading}</p>
-        <p>{error}</p>
-        <ul>
-            {users.map(item => (
-                <li key={item.id.value}>
-                    {item.name.title} {' '}
-                    {item.name.first} {' '}
-                    {item.name.last} {' '}
-                </li>
-            ))}
-        </ul>
+      <p>{isLoading}</p>
+      <p>{error}</p>
+      <ul>
+        {users.map((item) => (
+          <li key={item.id.value}>
+            {item.name.title} {item.name.first} {item.name.last}{" "}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Users;
--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,43 +1,44 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    users: [],
-    isLoading: false,
-    error: undefined
-}
+  users: [],
+  isLoading: false,
+  error: undefined,
+};
 
-export const fetchUsers = createAsyncThunk('users/fetchUsers', async() => {
-    try {
-        return fetch('https://randomuser.me/api/?results=5')
-        .then(resp => resp.json());
-    } catch (error) {
-        return error;
-    }
-})
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  try {
+    return fetch("https://randomuser.me/api/?results=5").then((resp) =>
+      resp.json()
+    );
+  } catch (error) {
+    return error;
+  }
+});
 
- export const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {
-        displayUser: () => {}
-    },
-    extraReducers: builder => {
-        builder.addCase(fetchUsers.pending, (state) => {
-            state.isLoading = true;
-        })
+export const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    displayUser: () => {},
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.isLoading = true;
+    });
 
-        builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.users = action.payload.results;
-            console.log(action.payload.results)
-        })
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.users = action.payload.results;
+      console.log(action.payload.results);
+    });
 
-        builder.addCase(fetchUsers.rejected, (state, action) => {
-            state.error = action.error;
-            state.isLoading = false;
-        })
-    }
-})
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.error = action.error;
+      state.isLoading = false;
+    });
+  },
+});
 
 export const { displayUser } = usersSlice.actions;
 
